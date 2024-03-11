0d61414c189bd090399a44b3a64367aee89e4783
carl0801 <118434635+carl0801@users.noreply.github.com>
2024-03-11 10:58:14 +0100

    changes in functions

--- src/IMU.cpp
+++ src/IMU.cpp
@@ -177,8 +177,8 @@ Angle complementaryFilter(Angle angle, RawData raw, Mag mag) {
   return angle;
 }
 
-//Get roll, pitch and yaw angle and alttitude from ToF sensor (degrees, degrees, degrees, mm)
-void IMU::getIMUData(double *roll, double *pitch, double *yaw, double *z1) {
+//Get roll, pitch and yaw angle (degrees)
+void IMU::getRotation(double *roll, double *pitch, double *yaw) {
 
   // read raw accel/gyro measurements from device
   accelgyro.getMotion6(&raw.ax, &raw.ay, &raw.az, &raw.gx, &raw.gy, &raw.gz);
@@ -189,14 +189,16 @@ void IMU::getIMUData(double *roll, double *pitch, double *yaw, double *z1) {
   // apply the complementary filter
   angle = complementaryFilter(angle, raw, mag);
 
-  angle.z1 = lidar.readRangeContinuousMillimeters();
-
   *roll = angle.roll;
   *pitch = angle.pitch;
   *yaw = angle.yaw;
-  *z1 = angle.z1;
 
 }
+
+//Get alttitude from ToF sensor
+void IMU::getAltitude(double *altitude) {
+  *altitude = lidar.readRangeContinuousMillimeters();
+};
 
 // Get the pressure from the BMP085 (Pa)
 void IMU::getPressure(double *pressure) {

8492880bca25e3ab09872d03f35be87c3a603a49
EmilRosenlund <116708131+EmilRosenlund@users.noreply.github.com>
2024-04-18 17:14:58 +0200

    update

--- AltitudeTorqueWeightPWM V2.py
+++ AltitudeTorqueWeightPWM V2.py
@@ -8,6 +8,7 @@ def calculate_lift(pwm_output):
 
 print("Starting simulation...")
 # Connect to PyBullet physics server
+Hover = True
 p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
@@ -21,7 +22,8 @@ p.setGravity(0, 0, gravity)
 # Load the URDF file
 planeId = p.loadURDF("plane.urdf")
 # Load URDF file
-drone_id = p.loadURDF("C:/Users/emilr/OneDrive/Documents/GitHub/Tricopter/Simulation/drone.urdf", [0, 0, 0], p.getQuaternionFromEuler([0, 0, 0]))
+startOrientation = np.pi/2+np.deg2rad(-120)
+drone_id = p.loadURDF("C:/Users/emilr/OneDrive/Documents/GitHub/Tricopter/Simulation/drone.urdf", [0, 0, 0], p.getQuaternionFromEuler([0, 0, startOrientation]))
 
 # Initialize variables for regulation
 integral_x = 0.0
@@ -30,18 +32,18 @@ integral_alti = 0.00  # Integral term for altitude control
 previous_error_x = 0.0
 previous_error_y = 0.0
 previous_error_alti = 0.0  # Previous error for altitude control
-kp = 0.8
+
 
 kp_2 = 0.08 #roll and pitch
 ki = 0.00   #roll and pitch
 kd = 0.12
 
-
-ki_alti = 0.02#0.01  # Ki for altitude control
-kd_alti = 1  # Kd for altitude control
+kp = 1.5
+ki_alti = 0.3#0.01  # Ki for altitude control
+kd_alti = 1.5  # Kd for altitude control
 target_roll = 0.0
 target_pitch = 0.0
-target_yaw = 0.0
+target_yaw = -0.25
 target_altitude = 3.0
 target_x = 0.0
 target_y = 0.0
@@ -64,18 +66,27 @@ yaw_offset = 0.005
 # Get the joint indices for the motors and wings
 num_joints = p.getNumJoints(drone_id)
 motor_joints = []
-wing_joint_index = -1
+wing_joints = []
 for jointIndex in range(num_joints):
     joint_info = p.getJointInfo(drone_id, jointIndex)
     joint_name = joint_info[1].decode("utf-8")  # Convert bytes to string
     print(joint_name)  # Print joint names for inspection
     if "motor" in joint_name:
         motor_joints.append(jointIndex)
-    elif "Revolute_5" in joint_name:
-        wing_joint_index = jointIndex
+    elif "Revolute" in joint_name:
+        wing_joints.append(jointIndex)
+
+# Color the wings for better visualization diffent colors
+colors = [[1, 0, 0, 1], [0, 1, 0, 1], [0, 0, 1, 1]]
+for jointIndex, color in zip(wing_joints, colors):
+    p.changeVisualShape(drone_id, jointIndex, rgbaColor=color)
+    
+print("First wing is red, second wing is green, third wing is blue")
+print("first wing is m1, second wing is m3, third wing is m2")
+
 
 #print("Motor joints:", motor_joints)
-print("Wing joint index:", wing_joint_index)
+print("Wing joints", wing_joints)
 
 # create data array
 data = []
@@ -90,12 +101,12 @@ p.addUserDebugParameter("ki_yaw", 0, 1, ki_yaw)
 p.addUserDebugParameter("kd_yaw", 0, 1, kd_yaw)
 
 
-kp_position_x = 0.02
+kp_position_x = 0.01
 ki_position_x = 0.000
-kd_position_x = 0.00005
+kd_position_x = 0.00000
 kp_position_y = 0.02
 ki_position_y = 0.000
-kd_position_y = 0.0001
+kd_position_y = 0.0000
 
 # Initialize variables for position control
 integral_position_x = 0.0
@@ -110,15 +121,16 @@ def invert_quaternion(quat):
 
 # Run simulation continuously
 try:
-    while True:
+    while Hover == True:
         # Extract position and orientation
         pos, orn = p.getBasePositionAndOrientation(drone_id)
+        global_orientation = orn
         orn = invert_quaternion(orn)
-        pitch = orn[0]
-        roll = orn[1]
+        roll = -orn[0]
+        pitch = -orn[1]
         yaw = -orn[2]
+        #print("global orientation: ", global_orientation, "local orientation: ", roll, pitch, yaw)
         # Position control
-
         error_x = (target_x - pos[0])
         error_y = -(target_y - pos[1])
 
@@ -130,32 +142,21 @@ try:
         derivative_position_x = (error_x - previous_error_position_x) / time_step
         derivative_position_y = (error_y - previous_error_position_y) / time_step
 
-        if error_x > 0:
-            # means that the actual position is negative
-            offset_pitch = 0
-        else:
-            offset_pitch= 0
+        x_error = kp_position_x * error_x + ki_position_x * integral_position_x + kd_position_x * derivative_position_x
+        y_error = kp_position_y * error_y + ki_position_y * integral_position_y + kd_position_y * derivative_position_y
 
-        if error_y > 0:
-            # means that the actual position is negative
-            offset_roll = 0
-        else:
-            offset_roll = 0.0
-        # Calculate control inputs
-        target_pitch = -(kp_position_y * error_y + ki_position_y * integral_position_y  + kd_position_y * derivative_position_y - offset_roll)
-        target_roll = -(kp_position_x * error_x + ki_position_x * integral_position_x + kd_position_x * derivative_position_x + offset_pitch)
-        #constrain the target pitch and roll to -0.25*pi to 0.25*pi
-        target_roll = np.clip(target_roll, -0.30*np.pi, 0.30*np.pi)
-        target_pitch = np.clip(target_pitch, -0.30*np.pi, 0.30*np.pi)
-        #print( kp_position_y * error_y + ki_position_y * integral_position_y  + kd_position_y * derivative_position_y )
-        #print("roll:", roll, "pitch:", pitch, "yaw:", yaw, "x:", pos[0], "y:", pos[1], "z:", pos[2])
-        #print("target_roll:", target_roll, "target_pitch:", target_pitch)
+        # define dynamics for the position control
+        additional_tilt_m1 = -x_error #+ y_error
+        additional_tilt_m2 = x_error #- y_error
+        additional_tilt_m3 = 0
+        #print("additional_tilt_m1: ", additional_tilt_m1, "additional_tilt_m2: ", additional_tilt_m2, "additional_tilt_m3: ", additional_tilt_m3)
+
         
 
         error_orientation_x = target_roll - roll
         error_orientation_y = target_pitch - pitch
         error_altitude = target_altitude - pos[2]  # Altitude error
-
+        print("error roll", error_orientation_x, "error pitch", error_orientation_y)
         #print("roll:", roll, "pitch:", pitch, "yaw:", yaw)
         # Calculate errors
         error_orientation_x = target_roll - roll
@@ -186,9 +187,9 @@ try:
         # Calculate control inputs
         
         U_alti = kp * error_altitude + ki_alti * integral_alti + kd_alti * derivative_alti  # Altitude control
-
-        U_r1 = -kp_2 * error_orientation_x + (-ki * integral_x) + (-kd * derivative_x)
-        U_r2 = kp_2 * error_orientation_x + (ki * integral_x) + (kd * derivative_x)
+        
+        U_r1 = kp_2 * error_orientation_x + (ki * integral_x) + (kd * derivative_x)
+        U_r2 = -kp_2 * error_orientation_x + (-ki * integral_x) + (-kd * derivative_x)
         U_p1 = -kp_2 * error_orientation_y + (-ki * integral_y) + (-kd * derivative_y)
         U_p2 = -kp_2 * error_orientation_y + (-ki * integral_y) + (-kd * derivative_y)
         U_p3 = kp_2 * error_orientation_y  + ki * integral_y + kd * derivative_y
@@ -203,7 +204,7 @@ try:
         pwm2 = np.clip(pwm2, 0, 180)
         pwm3 = np.clip(pwm3, 0, 180)
         
-        print("PWM1: ", pwm1, "PWM2: ", pwm2, "PWM3: ", pwm3)
+        #print("PWM1: ", pwm1, "PWM2: ", pwm2, "PWM3: ", pwm3)
         output1 = calculate_lift(pwm1)+2
         output3 = calculate_lift(pwm2)+2
         output2 = calculate_lift(pwm3)+2
@@ -233,7 +234,13 @@ try:
         #tilt_angle = np.clip(tilt_angle, np.radians(-180.0), np.radians(180.0))  # Adjust range as needed
         # Set tilt angle for the wing
         #print("tilt_angle: ", tilt_angle)
-        p.setJointMotorControl2(drone_id, wing_joint_index, p.POSITION_CONTROL, targetPosition=tilt_angle)
+
+        tilt_angles = [tilt_angle+additional_tilt_m1, tilt_angle+additional_tilt_m3, tilt_angle+additional_tilt_m2]
+        #print("tilt_angles: ", tilt_angles)
+
+        # Apply the tilt angles to all the wings
+        for jointIndex, tilt_angle in zip(wing_joints, tilt_angles):
+            p.setJointMotorControl2(drone_id, jointIndex, p.POSITION_CONTROL, targetPosition=tilt_angle)
 
         # save all errors and motor output to data
         
@@ -244,7 +251,7 @@ try:
         previous_error_y = error_orientation_y
         previous_error_alti = error_altitude
         previous_error_yaw = yaw_error
-        p.resetDebugVisualizerCamera(2.0, 60.0, -40, pos)
+        p.resetDebugVisualizerCamera(1.0, 60.0, -40, pos)
         
         #show current yaw in the gui
          
